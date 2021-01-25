dafd699434cb4083fc9db6d3dbe69ed442434d28
larwon <74400003+larwon@users.noreply.github.com>
2021-01-26 00:35:38 +0900

    Add files via upload

---
+++ algo_01-03.py
@@ -0,0 +1,23 @@
+#algo_01
+# a = 10
+# b = 20
+# total = a + b
+# print(total)
+# print("sum:" + str(total))
+
+
+# algo_02
+# idnum = 123456
+# name = "Hong kildong"
+# print("id number:" + str(idnum))
+# print("name:" + name)
+
+
+# algo_03
+# os1 = "window"
+# os2 = "linux"
+# temp = os1
+# os1 = os2
+# os2 = temp
+# print("os1:" + os1)
+# print("os2:" + os2)
