34b4aa5f2a755d8835ffc4fc704d1afd058c0c67
larwon <74400003+larwon@users.noreply.github.com>
2021-01-27 00:33:13 +0900

    Add files via upload

---
+++ algo_04-06.py
@@ -0,0 +1,23 @@
+# algo_04
+# price = 1000
+# sale = 1300 * 25
+# profit = (1300-1000) * 25
+# print("총 판매액:" + str(sale))
+# print("판매이익:" + str(profit))
+
+
+# algo_05
+# kor = 89
+# math = 98
+# eng = 87
+# sci = 88
+# total = kor + math + eng + sci
+# print("홍길동의 평균은:" + str(total/4))
+
+
+# algo_06
+# r = 5
+# l = 2 * 3.14 * 5
+# s = 3.14 * 5**2 
+# print("반지름이 5인 원의 둘레는:" + str(l))
+# print("반지름이 5인 원의 넓이는:" + str(s))
