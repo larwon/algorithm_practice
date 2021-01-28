2d66f53c80c1d4cc3a1629c2ddff0f88d34377c6
larwon <74400003+larwon@users.noreply.github.com>
2021-01-28 09:53:55 +0900

    Add files via upload

---
+++ algo_07-09.py
@@ -0,0 +1,29 @@
+# algo_07
+# age = 65
+# if age >= 65:
+#     print("True")
+# else:
+#     print("False")
+
+
+# algo_08
+# age = 18
+# if age < 18:
+#     print("True")
+# else:
+#     print("False")
+
+# if age <= 18:
+#     print("True")
+# else:
+#     print("False")
+
+
+# algo_09
+# score1 = 90
+# score2 = 50
+# total = score1 + score2
+# if total < 180:
+#     print("True")
+# else:
+#     print("False")
