6012f402cc8760a708a5498c43d7d75d381e83ce
larwon <74400003+larwon@users.noreply.github.com>
2021-01-29 11:17:51 +0900

    Add files via upload

---
+++ algo_10-15.py
@@ -0,0 +1,71 @@
+# algo_10
+# age1 = 60
+# age2 = 40
+# age3 = 50
+# age4 = 40
+# total1 = age1 + age2
+# total2 = age3 + age4
+# if total1 == 100:
+#     print("True")
+# else:
+#     print("False")
+
+# if total2 != 100:
+#     print("True")
+# else:
+#     print("False")
+
+
+# algo_11
+# a = input("당신의 학점은:")
+# b = float(a)
+# if b >= 4.0:
+#     print("해외 연수의 기회부여")
+# else:
+#     print("죄송합니다")
+
+
+# algo_12
+# kor = int(input("국어 점수:"))
+# eng = int(input("영어 점수:"))
+# math = int(input("수학 점수:"))
+# avg = (kor + eng + math) / 3
+# print(avg)
+# if avg >= 95:
+#     print("당신의 평균점수는" + str(avg) + "입니다") 
+#     print("축하드립니다, A+입니다")
+#     print("감사합니다")
+# else:
+#     print("감사합니다")
+
+
+# algo_13
+# age = int(input("나이를 입력하세요:"))
+# if age >= 65:
+#     print("경로 우대 승차권 발급  가능")
+# else:
+#     print("일반 승차권 발급 가능")
+# print("이용해주셔서 감사합니다")
+
+
+# algo_14
+# number1 = int(input("첫번째 숫자를 입력해 주세요"))
+# number2 = int(input("두번째 숫자를 입력해 주세요"))
+# if number1 > number2:
+#     print("큰수는"+str(number1)+"이고", "작은수는" +str(number2)+ "입니다")
+# else:
+#     print("큰수는"+str(number2)+"이고", "작은수는" +str(number1)+ "입니다")
+
+
+# algo_15
+# grade = int(input("점수를 입력해주세요:"))
+# if grade >= 90:
+#     print("A입니다")
+# elif grade >= 80:
+#     print("B입니다")
+# elif grade >= 70:
+#     print("C입니다")
+# elif grade >= 60:
+#     print("D입니다")
+# else:
+#     print("F입니다")
