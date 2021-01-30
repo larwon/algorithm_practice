f69b6eff9ff7fc33c96e42b21bbac556374706da
larwon <74400003+larwon@users.noreply.github.com>
2021-01-30 13:13:01 +0900

    Add files via upload

---
+++ algo_16-20.py
@@ -0,0 +1,56 @@
+# algo_16
+# number1 = int(input("첫번째 숫자를 입력해주세요:"))
+# number2 = int(input("두번째 숫자를 입력해주세요:"))
+# if number1 > number2:
+#     print("큰수는" +str(number1)+ "이고", "작은수는" +str(number2)+ "입니다")
+# elif number1 == number2:
+#     print("두 수가 같습니다")
+# else:
+#     print("큰수는" +str(number2)+ "이고", "작은수는" +str(number1)+ "입니다")
+
+
+# algo_17
+# grade = int(input("점수를 입력해주세요:"))
+# if grade >= 60:
+#     if grade >= 70:
+#         if grade >= 80:
+#             if grade >=90:
+#                 print("A")
+#             else:
+#                 print("B")
+#         else:
+#             print("C")    
+#     else:
+#         print("D")    
+# else:
+#     print("F")
+
+
+# algo_18
+# name1 = input("첫번째 사람의 이름을 입력하세요:")
+# name2 = input("두번째 사람의 이름을 입력하세요:")
+# if name1 > name2:
+#     print(name2)
+#     print(name1)
+# else:
+#     print(name1)
+#     print(name2)
+
+
+# algo_19
+# age = int(input("나이를 입력하세요: "))
+# if age != 19:
+#     print("19세가 아닙니다")
+
+
+# algo_20
+# number1 = int(input("첫번째 숫자를 입력하세요: "))
+# number2 = int(input("두번째 숫자를 입력하세요: "))
+# sum = number1 + number2
+# mul = number1 * number2
+# if sum == mul:
+#     print("합과 곱이 같습니다")
+# elif mul > sum:
+#     print("곱이 합보다 큽니다")
+# else:
+#     print("합이 곱보다 큽니다")
