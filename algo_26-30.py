73bc1028e486ab8ce0874ae7938de997a2b993df
larwon <74400003+larwon@users.noreply.github.com>
2021-02-02 23:07:24 +0900

    Add files via upload

---
+++ algo_26-30.py
@@ -0,0 +1,47 @@
+# algo_26
+# height = int(input("키를 입력해주세요:  "))
+# weight = int(input("몸무게를 입력해주세요:  "))
+# a = height - weight
+# if (100 < a) and (a < 110):
+#     print("좋습니다")
+# else:
+#     print("나쁩니다")
+
+
+# algo_27
+# sex = input("성별을 입력해주세요(m or f):  ")
+# age = int(input("나이를 입력해주세요:  "))
+# if (sex == "m") and (age >= 18):
+#     print("좋습니다")
+# else:
+#     print("나쁩니다")
+
+
+# algo_28
+# sub1 = int(input("첫번째 과목의 점수를 입력해주세요:  "))
+# sub2 = int(input("두번째 과목의 점수를 입력해주세요:  "))
+# sub3 = int(input("세번째 과목의 점수를 입력해주세요:  "))
+# sub4 = int(input("네번째 과목의 점수를 입력해주세요:  "))
+# if (sub1 >= 90) and (sub2 >= 90) and (sub3 >= 90) and (sub4 >= 90):
+#     print("좋습니다")
+# else:
+#     print("나쁩니다")
+
+
+# algo_29
+# addr = input("주소를 입력해주세요:  ")
+# if (addr == "경기도") or (addr == "서울"):
+#     print("좋습니다")
+# else:
+#     print("나쁩니다")
+
+
+# algo_30
+# sub1 = int(input("첫번째 과목 등급을 적어주세요:  "))
+# sub2 = int(input("두번째 과목 등급을 적어주세요:  "))
+# sub3 = int(input("세번째 과목 등급을 적어주세요:  "))
+# sub4 = int(input("네번째 과목 등급을 적어주세요:  "))
+# if (sub1 == 1) or (sub2 == 1) or (sub3 == 1) or (sub4 == 1):
+#     print("좋습니다")
+# else:
+#     print("나쁩니다")
