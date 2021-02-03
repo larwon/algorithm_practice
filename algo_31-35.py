becd523455253e5c128a2e99a6e154071e934de5
larwon <74400003+larwon@users.noreply.github.com>
2021-02-03 23:17:45 +0900

    Add files via upload

---
+++ algo_31-35.py
@@ -0,0 +1,39 @@
+# algo_31
+# num = int(input("숫자를 입력해주세요:  "))
+# if num != 0:
+#     print("좋습니다")
+# else:
+#     print("나쁩니다")
+
+
+# algo_32
+# score1 = int(input("첫번째 과목의 점수를 입력해주세요:  "))
+# score2 = int(input("두번째 과목의 점수를 입력해주세요:  "))
+# if (score1 < 60) and (score2 < 60):
+#     print("재수강 대상입니다")
+# else:
+#     print("재수강 대상이 아닙니다")
+
+
+# algo_33
+# score1 = int(input("첫번째 과목의 점수를 입력해주세요:  "))
+# score2 = int(input("두번째 과목의 점수를 입력해주세요:  "))
+# sum = score1 + score2
+# if (150 < sum) and (sum < 170) and (score1 > 60) and (score2 > 60):
+#     print("보통성적입니다")
+
+
+# algo_34
+# score = int(input("신체검사 점수를 입력해주세요:  "))
+# age = int(input("나이를 입력해주세요:  "))
+# gender = input("성별을 입력해주세요:  ")
+# if (score > 80) and (age == 18) and (gender == "m"):
+#     print("현역입영대상입니다")
+
+
+# algo_35
+# spell = input("영문자 한 문자를 입력해주세요:  ")
+# if (("A" <= spell) and (spell <= "Z")) or (("a" <= spell) and (spell <= "z")):
+#     print("입력해주신 문자는" +spell+ "입니다")
+# else:
+#     print("이것은 문자가 아닙니다" +spell)
