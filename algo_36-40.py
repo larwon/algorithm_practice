a88e5a2808d1f2e54eb288fb829aadedaabb8f9a
larwon <74400003+larwon@users.noreply.github.com>
2021-02-04 23:15:24 +0900

    Add files via upload

---
+++ algo_36-40.py
@@ -0,0 +1,45 @@
+# algo_36
+# score1 = int(input("첫번째 영어 점수를 입력해주세요:  "))
+# score2 = int(input("두번째 영어 점수를 입력해주세요:  "))
+# score3 = int(input("세번째 영어 점수를 입력해주세요:  "))
+# toeic = int(input("토익 점수를 입력해주세요:  "))
+# sum = score1 + score2 + score3
+# avg = sum / 3
+# if (avg >= 80) or (toeic >= 700):
+#     print("합격입니다")
+# else:
+#     print("불합격입니다")
+
+
+# algo_37
+# num = int(input("숫자를 입력해주세요:  "))
+# if (num % 5 != 0):
+#     print("5의 배수가 아닙니다")
+# else:
+#     print("5의 배수입니다")
+
+
+# algo_38
+# score1 = int(input("첫번째 과목의 점수를 입력해주세요:  "))
+# score2 = int(input("두번째 과목의 점수를 입력해주세요:  "))
+# sum = score1 + score2
+# if  ((score1 > 80) and (score2 > 80)) or (sum > 160):
+#     print("우수학생입니다")
+
+
+# algo_39
+# num1 = int(input("첫번째 숫자를 입력해주세요:  "))
+# num2 = int(input("두번째 숫자를 입력해주세요:  "))
+# if (num1 % 2 == 0) and (num2 % 2 == 0):
+#     print(str((int(num1)+int(num2))) + "입니다")
+
+
+# algo_40
+# age = int(input("나이를 입력해주세요:  "))
+# if age >= 65:
+#     if (age < 0) or (age > 200):
+#         print("나이를 잘못입력하셨습니다.")
+#     else:
+#         print("경로우대 대상입니다")
+# else:
+#     print("대상이 아닙니다") 
