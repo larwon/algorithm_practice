bdfc3dd981cd2c882fa2eea47a0915fd39454139
larwon <74400003+larwon@users.noreply.github.com>
2021-02-05 23:09:38 +0900

    Add files via upload

---
+++ algo_41-45.py
@@ -0,0 +1,60 @@
+# algo_41
+# month = int(input("현재 개월을 입력해주세요:  "))
+# if (3 <= month) and (month <= 5):
+#     print("현재 봄입니다")
+# elif (6 <= month) and (month <= 8):
+#     print("현재 여름입니다")
+# elif (9 <= month) and (month <= 11):
+#     print("현재 가을입니다")
+# elif ((11 <= month) and (month <= 12)) or (month <= 1):
+#     print("현재 겨울입니다")
+# else:
+#     print("잘못입력하셨습니다")
+
+
+# algo_42
+# position = int(input("당신의 직급을 입력해주세요:  "))
+# age = int(input("당신의 나이를 입력해주세요:  "))
+# if (7 <= position) and (position <= 8) and (40 <= age) and (age <= 49):
+#     print("당신은 연금80% 대상자입니다")
+# elif (5 <= position) and (position <= 6) and (50 <= age) and (age <= 59):
+#     print("당신은 연급100% 대상자입니다")
+# else:
+#     print("당신은 대상자가 아닙니다")
+
+
+# algo_43-1
+# num = int(input("숫자를 입력해주세요:  "))
+# if num >= 1:
+#     print(str(num)+"양수입니다")
+# elif num == 0:
+#     print(str(num)+"0입니다")
+# else:
+#     print(str(num)+"음수입니다")
+# algo_43-2
+# num = int(input("숫자를 입력해주세요:  "))
+# if num >= 1:
+#     print(str(num)+"양수입니다")
+# if num == 0:
+#     print(str(num)+"0입니다")
+# if num <= -1:
+#     print(str(num)+"음수입니다")
+
+
+# algo_44
+# num1 = int(input("첫번째 숫자를 입력해주세요:  "))
+# cal = input("연산자를 입력해주세요 (+ or -):  ")
+# num2 = int(input("두번째 숫자를 입력해주세요:  "))
+# if (cal == "+"):
+#     print(num1 + num2)
+# elif (cal == "-"):
+#     print(num1 - num2)
+# else:
+#     print("해당되는 연산은 없습니다")    
+
+
+# algo_45
+# count = 1
+# while count <= 10:
+#     print("**********")
+#     count = count + 1
